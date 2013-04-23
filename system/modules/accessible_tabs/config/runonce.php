c3bd99eac76174a192637d869e028ec72e1f6cba
yfridelance <yves@fridelance.ch>
2013-04-23 11:31:33 +0200

    update runonce.php

--- system/modules/accessible_tabs/config/runonce.php
+++ system/modules/accessible_tabs/config/runonce.php
@@ -71,7 +71,7 @@ class AccessibleTabsRunonce extends Controller
                 DCA_TL_Accessible_Tabs::getInstance()->loadSettings($name);
 
                 // migrate Database
-                $this->migrateDatabase(true);
+                $this->migrateDatabase();
             }
 
         }
@@ -123,32 +123,32 @@ class AccessibleTabsRunonce extends Controller
         );
     }
 
-    protected function migrateDatabase($debug = false)
+    protected function migrateDatabase()
     {
 
         //Add Fields
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_type` varchar(32) NOT NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_tabhead` varchar(2) NOT NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_position` varchar(32) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_sync_heights` char(1) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_save_state` char(1) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_auto_anchor` char(1) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_pagination` char(1) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_fx` varchar(32) NOT NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_fxspeed` varchar(32) NOT NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_wrapper_class` varchar(32) NOT NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_current_class` varchar(32) NOT NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_tabhead_class` varchar(32) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_tabbody` varchar(32) NOT NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_tabs_list_class` varchar(32) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_first_nav_item_class` varchar(32) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_last_nav_item_class` varchar(32) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_clearfix_class` varchar(32) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_title` varchar(255) NOT NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_anchor` varchar(255) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_css_class_available` char(1) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_wrap_inner_nav_links` char(1) NULL default ''");
-        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_sync_height_method_name` varchar(32) NOT NULL default ''");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_type` varchar(32) NOT NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_tabhead` varchar(2) NOT NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_position` varchar(32) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_sync_heights` char(1) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_save_state` char(1) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_auto_anchor` char(1) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_pagination` char(1) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_fx` varchar(32) NOT NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_fxspeed` varchar(32) NOT NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_wrapper_class` varchar(32) NOT NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_current_class` varchar(32) NOT NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_tabhead_class` varchar(32) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_tabbody` varchar(32) NOT NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_tabs_list_class` varchar(32) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_first_nav_item_class` varchar(32) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_last_nav_item_class` varchar(32) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_clearfix_class` varchar(32) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_title` varchar(255) NOT NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_anchor` varchar(255) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_css_class_available` char(1) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_wrap_inner_nav_links` char(1) NULL default '';");
+        $this->DB->execute("ALTER TABLE `tl_content` ADD `accessible_tabs_sync_height_method_name` varchar(32) NOT NULL default '';");
 
         if ($this->DB->fieldExists('accessible_tabs_type', 'tl_content'))
         {
@@ -184,14 +184,7 @@ class AccessibleTabsRunonce extends Controller
                         'accessible_tabs_sync_height_method_name'   => $rs->fry_tabsType == 'Start' ? $GLOBALS['TL_CONFIG']['accessible_tabs_sync_height_method_name'] : null,
                     );
 
-                    if($debug)
-                    {
-                        var_dump($this->DB);
-                    }
-                    else
-                    {
-                        $this->DB->prepare($query)->set($set)->execute($rs->id);
-                    }
+                    $this->DB->prepare($query)->set($set)->execute($rs->id);
                 }
             }
         }
