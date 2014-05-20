ad4792ecc82a788a332fe6b8421b08793d73e9f0
Yves Fridelance <yves.fridelance@me.com>
2014-05-20 13:21:26 +0200

    Update Languagefile

--- src/system/modules/fry_accessible_tabs/languages/en/default.php
+++ src/system/modules/fry_accessible_tabs/languages/en/default.php
@@ -15,8 +15,11 @@
 /**
  * ContentElement
  */
-$GLOBALS['TL_LANG']['CTE']['accessible_tabs']	           = 'Accessible Tabs';
-$GLOBALS['TL_LANG']['CTE']['accessible_tabs_start'][0]     = 'Wrapper start';
-$GLOBALS['TL_LANG']['CTE']['accessible_tabs_separator'][0] = 'New Tab';
-$GLOBALS['TL_LANG']['CTE']['accessible_tabs_stop'][0]      = 'Wrapper stop';
+$GLOBALS['TL_LANG']['CTE']['accessible_tabs']	           	= 'Accessible Tabs';
+$GLOBALS['TL_LANG']['CTE']['accessible_tabs_start'][0]     	= 'Wrapper start';
+$GLOBALS['TL_LANG']['CTE']['accessible_tabs_start'][1]		= 'Generates the opening part of the Accessible Tabs wrapper.';
+$GLOBALS['TL_LANG']['CTE']['accessible_tabs_separator'][0] 	= 'New Tab';
+$GLOBALS['TL_LANG']['CTE']['accessible_tabs_separator'][1] 	= 'Generate a new Tab.';
+$GLOBALS['TL_LANG']['CTE']['accessible_tabs_stop'][0]      	= 'Wrapper stop';
+$GLOBALS['TL_LANG']['CTE']['accessible_tabs_stop'][1]      	= 'Generates the closing part of the Accessible Tabs wrapper.';
 
